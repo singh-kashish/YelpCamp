24acbdc44c486093b3b85e87a76359a64b45d85f
Kashish Singh <ks1147@srmist.edu.in>
2020-09-23 19:09:27 +0000

    changed port in app.js

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ var commentRoutes        =      require("./routes/comments"),
 app.use("/campgrounds/:id/comments",commentRoutes);
 app.use(authRoutes);
 app.use("/campgrounds",campgroundRoutes);
-app.listen(3000,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
 	console.log("YelpCamp server started");
 })
