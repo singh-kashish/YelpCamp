f8a96516b8fcd13cabe2025a6eee3dcc0adf1daf
Kashish Singh <ks1147@srmist.edu.in>
2020-09-24 11:03:15 +0000

    add time functionality to campground creation and comment

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(function(req,res,next){
 	res.locals.currentUser=req.user;
 	res.locals.error=req.flash("error");
 	res.locals.success=req.flash("success");
+	app.locals.moment=require("moment");
 	next();
 });
 
--- models/campground.js
+++ models/campground.js
@@ -5,6 +5,7 @@ var campgroundSchema=new mongoose.Schema({
 	price:String,
 	image:String,
 	description:String,
+	createdAt:{type:Date,default:Date.now},
 	author:{
 		id:{type:mongoose.Schema.Types.ObjectId,
 		   ref:"User"},
